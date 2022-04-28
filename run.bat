73ce479aa8705159dd5b77b8ae8d42a4b4a24588
unknown <krishnamoorthy2611@yahoo.co.in>
2022-04-28 17:10:36 +0530

    added few changes in pom xml

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd E:\Ecilpse\ECLIPSE-WORKSPAC\Workspace\CucumberTesting
+mvn clean install
--- src/test/java/testRunner/Runner.java
+++ src/test/java/testRunner/Runner.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {".//Features/"}, glue = "stepDefinitions", dryRun =false, monochrome = true, plugin = {
-		"pretty", "html:test-output" }, tags={"@Sanity,@Regression"})
+		"pretty", "html:test-output" }, tags={"@Sanity"})
 public class Runner {
 
 }
